d51c7dba3ff75dc22d5a43d1b503a4602d6503b1
DeeMak13 <dima.m@active-bridge.com>
2018-10-30 18:09:14 +0200

    Remove run command

--- lib/superbot/local/cli/local_command.rb
+++ lib/superbot/local/cli/local_command.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'local/version_command'
-require_relative 'local/run_command'
 require_relative 'local/easy_mode_command'
 
 module Superbot
   module CLI
     class LocalCommand < Clamp::Command
-      subcommand ['run'], "Run tests from local directory", Local::RunCommand
       subcommand ['version'], "Superbot local version", Local::VersionCommand
       subcommand ['easymode'], "Superbot UI test constructor", Local::EasyModeCommand
 
